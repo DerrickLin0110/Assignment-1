411f01c1d5754eee6fa81d31f9b899ce7c7e1dc2
Derrick's HomeDestop <derrick.lin@student.tdsb.on.ca>
2025-02-12 17:34:20 -0500

    let repeat 5 time and stop

---
+++ v09.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int i=0;
+        While (i<7){
+        System.out.println(input); 
+    }
+}
+}

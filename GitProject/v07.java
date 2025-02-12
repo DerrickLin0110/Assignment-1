b13276badff8ddcd364393ad38e31b6dcbacb00b
Derrick's HomeDestop <derrick.lin@student.tdsb.on.ca>
2025-02-12 17:28:39 -0500

    add while loop replace for loop in NB branch

---
+++ GitProject/v07.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int i=7;
+        for (int i = 0; i < 7; i++){
+        System.out.println(input); 
+    }
+}
+}
